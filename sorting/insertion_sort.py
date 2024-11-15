7df89766dfa531a42895d3a34b75950e0fc5a604
Paula Amaya <prdamaya@gmail.com>
2024-11-14 20:28:53 -0700

    Finished insertion sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -4,7 +4,7 @@ def insertion_sort(xs: List[int]) -> List[int]:
     for i in range(1, len(xs)):
         key = xs[i]
         j = i - 1
-        while j > 0 and xs[j] > key:
-            xs[j], xs[j - 1] = xs[j - 1], xs[j]
+        while j >= 0 and xs[j] > key:
+            xs[j], xs[j + 1] = xs[j + 1], xs[j]
             j -= 1
     return xs
